ce96432468c280d917621a48885c953fc50279ad
Noa <nonistein@gmail.com>
2016-12-06 21:11:09 +0200

    201612062110 added mini RENDER PIC

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('Hello Meme');
+
+var gImages = [
+    { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
+    { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
+    { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] }
+];
+
+
+function RenderImages() {
+    var strHTML;
+    var img = document.querySelector('.my-img');
+    for (var i = 0; i < gImages.length; i++) {
+        strHTML += '<img class= "my-img" src="' + gImages[i].url + '"></img>';
+    }
+    img.innerHTML = strHTML;
+}
+
+RenderImages();
