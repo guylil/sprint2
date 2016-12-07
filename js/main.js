df26b6bf5241941bf5899ff38024e7fc44a0090b
Noa <nonistein@gmail.com>
2016-12-07 09:20:03 +0200

    2016-12/7-9:19 added hexagon

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 var gImages = [
     { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
-    { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
-    { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] }
+    { id: '2', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
+    { id: '3', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] }
 ];
 
 
@@ -11,7 +11,8 @@ function RenderImages() {
     var strHTML='';
     var img = document.querySelector('.my-imges');
     for (var i = 0; i < gImages.length; i++) {
-        strHTML += '<img class="my-img" src="' + gImages[i].url + '">';
+        strHTML+='<div class="hexagon2" style="background-image:url('+ gImages[i].url+');"><div class="hexTop"></div><div class="hexBottom"></div>'
+        // strHTML += '<img class="my-img" src="' + gImages[i].url + '">';
     }
     console.log('strHTML: ',strHTML);
     
