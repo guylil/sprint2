7271d13d0af188969d47eca7f08e7751eb786d1a
guylil <guylilian@gmail.com>
2016-12-07 20:44:41 +0200

    canvas js changes

--- js/canvas.js
+++ js/canvas.js
@@ -2,18 +2,22 @@
 console.log('hello canvas');
 
 function canvasPaint() {
+    /// TO DO : FIX THIS FUNCTION SO IT WILL BE DINAMIC
+    /// >>height >>width >> drawImage
     var elCanvas = document.getElementById('canvas');
     // elCanvas.display='none';
-    canvas.width =800;
-    canvas.heigth=600;
+    // canvas.height = 300;
+    // canvas.width = 400;
     console.log(elCanvas);
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#0e70d1';
+    // ctx.fillStyle = '#0e70d1';
     var img = new Image();
-    img.src = '../assets/img/5.png'
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, 568, 360);
-        ctx.fillText("print on Canvas", 50, 300);
+    img.src = '../assets/img/3.png'
+    img.onload = function() {
+        ctx.drawImage(img, 10, 10, 390, 290);
+        ctx.fillText("Noa's & Guy's MemeGenerator", 10, 300);
+        ctx.fillStyle = '#fff';
+        // ctx.restore();
     };
     // ctx.fillRect(0, 0, canvas.width, canvas.height);
     // document.getElementById('title').addEventListener('keyup', function () {
@@ -21,11 +25,9 @@ function canvasPaint() {
     //     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //     ctx.fillRect(0, 0, canvas.width, canvas.height);
     //     var stringTitle = document.getElementById('title').value;
-    //     ctx.fillStyle = '#fff';
     //     ctx.font = '60px sans-serif';
     //     var text_title = stringTitle;
     //     ctx.fillText(stringTitle, 15, canvas.height / 2 + 35);
-    //     ctx.restore();
     // }); 
 }
 canvasPaint();
