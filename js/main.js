d90e70b5045f5e9071ede563d1698fed3bb1084f
Noa <nonistein@gmail.com>
2016-12-07 11:10:38 +0200

    2016/7-12/11:10-added img and injectetnew hexa

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 'use strict';
 
 var gImages = [
-    { id: '1', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
-    { id: '2', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] },
-    { id: '3', url: 'https://imgflip.com/s/meme/Black-Girl-Wat.jpg', keywords: ['bla,sle'] }
+    { id: '1', url: '/assets/img/best-crying-jordan-memes.jpg', keywords: ['bla,sle'] },
+    { id: '2', url: '/assets/img/Captain-Picard-Facepalm.jpg', keywords: ['bla,sle'] },
+    { id: '3', url: '/assets/img/Disaster-Girl.jpg', keywords: ['bla,sle'] },
 ];
 
 
 function RenderImages() {
-    var strHTML='';
     var img = document.querySelector('.my-imges');
+    var strHTML = '';
     for (var i = 0; i < gImages.length; i++) {
-        strHTML+='<div class="hexagon2" style="background-image:url('+ gImages[i].url+');"><div class="hexTop"></div><div class="hexBottom"></div>'
-        // strHTML += '<img class="my-img" src="' + gImages[i].url + '">';
+        var hex = '<div class="hexagon2" style="background-image:url(' + gImages[i].url + ');">' +
+            '<div class="hexTop"></div>' +
+            '<div class="hexBottom"></div>' +
+            '</div>';
+        strHTML += hex;
     }
-    console.log('strHTML: ',strHTML);
-    
     img.innerHTML = strHTML;
+    console.log('strHTML: ', strHTML);
+
 }
 
 RenderImages();
