454f1870015d9ddb3d0400908072c1856b6529a9
guylil <guylilian@gmail.com>
2016-12-07 15:54:14 +0200

    minor changes 15:54

--- js/search.js
+++ js/search.js
@@ -1,23 +1,12 @@
 'use strict';
-console.log('hello search');
-
-
-
-console.log(gImages);
-
-// memeSearch(gImages, 'e');
+// console.log('hello search');
 
 function memeSearch(images) {
     var pressed = document.getElementsByName('search-box');
-console.log(gImages);
     
     var text = pressed[0].value;
-console.log(text);
     var res = images.filter(function (image) {
         return (image.keywords.join(',').indexOf(text) + 1)
     })
-    console.log(res);
-    // gImages = res;// FIX ME!!!!!!
-    // RenderImages();// FIX ME!!!!!
     return res
 }
