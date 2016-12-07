c2a183ca7605385316d9251415ffc2466680a555
guylil <guylilian@gmail.com>
2016-12-07 23:01:14 +0200

    canvas functions 20161207-23:00

--- js/canvas.js
+++ js/canvas.js
@@ -1,33 +1,60 @@
 'use strict';
 console.log('hello canvas');
 
-function canvasPaint() {
-    /// TO DO : FIX THIS FUNCTION SO IT WILL BE DINAMIC
-    /// >>height >>width >> drawImage
+function canvasPaint(meme = '../assets/img/3.png', canvasHeight = 300, canvasWidth = 400, inputTopText = "not sure if...", inputBottomText = "or if....", topTextHeight = 7, bottomTextHeight = 7, topTextAlign = 40, bottomTextAlign = 150, textFontSize = 30, topTextColor = 'white', bottomTextColor = 'red', transText = 0.5) {
     var elCanvas = document.getElementById('canvas');
-    // elCanvas.display='none';
-    // canvas.height = 300;
-    // canvas.width = 400;
-    console.log(elCanvas);
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
     var ctx = canvas.getContext('2d');
     // ctx.fillStyle = '#0e70d1';
     var img = new Image();
-    img.src = '../assets/img/3.png'
+    img.src = meme;
     img.onload = function() {
-        ctx.drawImage(img, 10, 10, 390, 290);
-        ctx.fillText("Noa's & Guy's MemeGenerator", 10, 300);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        // ctx.save();
+        ctx.font = textFontSize + 'pt David';
+        ctx.fillStyle = 'rgba(0,0,0,' + transText + ')';
+        ctx.fillRect(0, 0, canvas.width, canvas.height / topTextHeight);
+        ctx.fillRect(0, canvas.height - canvas.height / bottomTextHeight, canvas.width, canvas.height / bottomTextHeight);
+        // ctx.save();
+        ctx.fillStyle = topTextColor;
+        ctx.fillText(inputTopText.toUpperCase(), topTextAlign, textFontSize + 3);
+        ctx.fillStyle = bottomTextColor;
+        ctx.fillText(inputBottomText.toUpperCase(), bottomTextAlign, canvas.height - 5);
+        ctx.font = '10pt Dvaid'
         ctx.fillStyle = '#fff';
+        ctx.fillText("Noa's & Guy's MemeGenerator", 10, 300);
         // ctx.restore();
+        // ctx.fillStyle = '#fff';
+        ctx.save();
+        // return ctx
     };
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // document.getElementById('title').addEventListener('keyup', function () {
-    //     ctx.save();
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //     var stringTitle = document.getElementById('title').value;
-    //     ctx.font = '60px sans-serif';
-    //     var text_title = stringTitle;
-    //     ctx.fillText(stringTitle, 15, canvas.height / 2 + 35);
-    // }); 
+    // var newImg=new Image();
+    // newImg.id="meme";
+    // newImg.src = elCanvas.toDataURL();
+    // document.getElementById('img_generated').appendChild(newImg);
+    // console.log(ctx);
+    // return res
 }
+function addLines() {
+    // var elCanvas = document.getElementById('canvas');
+    // var ctx = canvas.getContext('2d');
+    // ctx.drawImage(res, 0, 0, 300, 400);
+    // = canvas.getContext('2d');
+    // ctx.restore();        
+    // ctx.font = '30pt Dvaid'
+    // ctx.fillStyle = '#fff';
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillText("Noa's & Guy's MemeGenerator", 10, 300);
+
+}
+// document.getElementById('title').addEventListener('keyup', function () {
+// ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+//     var stringTitle = document.getElementById('title').value;
+//     ctx.font = '60px sans-serif';
+//     var text_title = stringTitle;
+//     ctx.fillText(stringTitle, 15, canvas.height / 2 + 35);
+// }); 
 canvasPaint();
+// addLines();
